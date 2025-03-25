4abb523389be0e63a65f009a32edd6366c9fc995
Glitch (glitch-hello-website) <none>
2025-03-25 00:49:46 +0000

    🌅👙 Checkpoint ./server.js:70685239/14

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use((req, res, next) => {
     const minute = time.getMinutes(); // JST Minute
 
  const blockedTime = (
-    //(hour >= 21 || hour < 7) || // 9 PM to 7 AM
-    // (hour >= 11 && hour < 16) || // 11 AM to 2 PM
+    (hour >= 21 || hour < 7) || // 9 PM to 7 AM
+     (hour >= 11 && hour < 16) || // 11 AM to 2 PM
     (hour >= 18 && hour < 18)   // 6 PM to 7 PM
 );
 
