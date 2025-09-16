eb120472726752f10b9f0820d50ec2aa2d33d802
scratchmasterj21 <john.limpiada@felice.ed.jp>
2025-09-17 08:36:36 +0900

    ddddd

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use((req, res, next) => {
     const minute = time.getMinutes(); // JST Minute
 
 const blockedTime = !(
-   (hour >= 7 && hour < 11) //||  // Morning play time
-  (hour >= 14 && hour < 18)// || // Afternoon play time
+   (hour >= 7 && hour < 11) ||  // Morning play time
+  (hour >= 14 && hour < 18) || // Afternoon play time
   (hour >= 19 && hour < 21)    // Evening play time
 );
 
